3e5970fddac81d5e7156ef75bb2545a51111e106
sharad9201 <sharad.rae19@gmail.com>
2021-03-05 14:06:51 +0545

    table and style css

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const dotenv= require('dotenv')
 const morgan = require('morgan')
+const bodyparser = require('body-parser')
+const path = require('path')
 
 const app = express()
 
@@ -11,8 +13,24 @@ const PORT = process.env.PORT||8080
 //morgan help to log request for a consle whenever we request
 app.use(morgan('tiny'))
 
+//parse request to body
+
+app.use(bodyparser.urlencoded({extended:true}))
+
+// set view engine
+
+app.set("view engine","ejs")
+// app.set("view",path.resolve(__dirname,"views/ejs"))
+
+//load assets
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+
+
 app.get('/',(req,res)=>{
-    res.send("crud application")
+    // res.send("crud application")
+    res.render('index')
 })
 
 app.listen(PORT,()=>{console.log(`server is running on http://localhost:${PORT}`)})
